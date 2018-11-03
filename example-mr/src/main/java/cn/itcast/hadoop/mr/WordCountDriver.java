de23496db98010ee8d426f3f4b6c91aba3754c9c
1064030249 <1064030249@qq.com>
2018-11-03 17:08:46 +0800

    文本单词计数

---
+++ example-mr/src/main/java/cn/itcast/hadoop/mr/WordCountDriver.java
@@ -0,0 +1,42 @@
+package cn.itcast.hadoop.mr;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 10640
+ * Date: 2018-11-03
+ * Time: 15:40
+ */
+public class WordCountDriver {
+    public static void main(String[] args) throws Exception{
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(WordCountDriver.class);
+
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.setInputPaths(job,"/wordcount/input");
+        FileOutputFormat.setOutputPath(job,new Path("/wordcount/output"));
+
+        boolean b = job.waitForCompletion(true);
+        System.exit(b?0:1);
+
+    }
+}
